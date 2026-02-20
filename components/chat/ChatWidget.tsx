ebcc1b8ee033815d8c51ae480ad7860848067ca9
Wilker Martins <dev.wilker@gmail.com>
2026-02-20 15:26:31 -0300

    20-02-2026  15:26

--- components/chat/ChatWidget.tsx
+++ components/chat/ChatWidget.tsx
@@ -71,7 +71,7 @@ export default function ChatWidget() {
     };
 
     return (
-        <div className="fixed bottom-6 right-6 z-9999 font-sans">
+        <div className="fixed bottom-24 right-6 z-9999 font-sans">
             <AnimatePresence>
                 {isOpen && (
                     <motion.div
